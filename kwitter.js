c20efcce288323e394b6714725a408576d40bc5f
tanish1243 <71543868+tanish1243@users.noreply.github.com>
2021-05-21 09:09:07 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,6 @@
 function adduser(){
     var Username= document.getElementById("username").value;
     localStorage.setItem("Username", Username);
+    
     window.location="kwitter_room.html";
  }
--- kwitter_room.js
+++ kwitter_room.js
@@ -1,1 +1,42 @@
 
+var firebaseConfig = {
+    apiKey: "AIzaSyBbk657CPROe7uq6MhqUnF4nmwHJKu92pE",
+    authDomain: "letschat2-9cae5.firebaseapp.com",
+    databaseURL: "https://letschat2-9cae5-default-rtdb.firebaseio.com",
+    projectId: "letschat2-9cae5",
+    storageBucket: "letschat2-9cae5.appspot.com",
+    messagingSenderId: "86285751519",
+    appId: "1:86285751519:web:af5acaa9013593263f056a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
+
+  function logout(){
+    window.location="index.html";
+    localStorage.removeItem("Username");
+    localStorage.removeItem("Room_name");
+  }
+  function addroom(){
+    var roomname= document.getElementById("roomname").value;
+    firebase.database().ref("/").child(roomname).update({
+          purpose: "adding roomname"
+    })  ;
+    localStorage.setItem("Room_Name",roomname );
+    window.location="kwitter_page.html";
+    var username =  localStorage.getItem("Username");
+    document.getElementById("usernamedisplay").innerHTML="Welcome "+ username+"!";
+
+     }
+     function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+      Room_names = childKey;
+     //Start code
+     console.log(Room_names);
+     var row="<div class='roomname'id="+ Room_names+"onclick='redirectToRoomName(this.id)'>#"+ Room_names +"</div><hr>"
+      document.getElementById("output").innerHTML+= row;
+
+     //End code
+     });});}
+     getData();
+     
